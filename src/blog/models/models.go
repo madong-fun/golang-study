41f18e4062c747277f1d5e03df4cc2aba5b43ff6
madong-fun <madong_2010@163.com>
2016-01-21 18:41:53 +0800

    golang study

--- src/blog/models/models.go
+++ src/blog/models/models.go
@@ -272,3 +272,64 @@ func GetAllReplies(tid string) (replies []*Comment, err error) {
 	_, err = qs.Filter("tid", tidNum).All(&replies)
 	return replies, err
 }
+
+func AddReply(tid, nickname, content string) error {
+	tidNum, err := strconv.ParseInt(tid, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	reply := &Comment{
+		Tid:     tidNum,
+		Name:    nickname,
+		Content: content,
+		Created: time.Now(),
+	}
+	o := orm.NewOrm()
+	_, err = o.Insert(reply)
+	if err != nil {
+		return err
+	}
+
+	topic := &Topic{Id: tidNum}
+	if o.Read(topic) == nil {
+		topic.ReplyTime = time.Now()
+		topic.ReplyCount++
+		_, err = o.Update(topic)
+	}
+	return err
+}
+
+func DeleteReply(rid string) error {
+	ridNum, err := strconv.ParseInt(rid, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	o := orm.NewOrm()
+
+	var tidNum int64
+	reply := &Comment{Id: ridNum}
+	if o.Read(reply) == nil {
+		tidNum = reply.Tid
+		_, err = o.Delete(reply)
+		if err != nil {
+			return err
+		}
+	}
+
+	replies := make([]*Comment, 0)
+	qs := o.QueryTable("comment")
+	_, err = qs.Filter("tid", tidNum).OrderBy("-created").All(&replies)
+	if err != nil {
+		return err
+	}
+
+	topic := &Topic{Id: tidNum}
+	if o.Read(topic) == nil {
+		topic.ReplyTime = replies[0].Created
+		topic.ReplyCount = int64(len(replies))
+		_, err = o.Update(topic)
+	}
+	return err
+}
